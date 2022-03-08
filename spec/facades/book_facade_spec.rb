8446e64da3e37e9b884e2232b4c699fcd3d3a628
Josh Walsh <87674632+jaw772@users.noreply.github.com>
2022-03-08 14:52:16 -0700

    Rewrite tests for part 1

--- spec/facades/book_facade_spec.rb
+++ spec/facades/book_facade_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
-require 'webmock/rspec'
+# require 'webmock/rspec'
 
 RSpec.describe BookFacade do
   it '::books' do
-    response = File.read('spec/fixtures/books.json')
-    stub_request(:get, 'http://openlibrary.org/search.json?title=denver&limit=5')
-      .to_return({
-                   status: 200,
-                   body: response
-                 })
+    # response = File.read('spec/fixtures/books.json')
+    # stub_request(:get, 'http://openlibrary.org/search.json?title=denver&limit=5')
+    #   .to_return({
+    #                status: 200,
+    #                body: response
+    #              })
     books = BookFacade.books("denver", "5")
     expect(books[:docs].first[:title]).to eq 'East of Denver'
     expect(books[:docs].last[:title]).to eq 'Water-quality assessment for Arvada Reservoir, Denver metropolitan area, Colorado'
--- spec/facades/weather_facade_spec.rb
+++ spec/facades/weather_facade_spec.rb
@@ -1,14 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe WeatherFacade do
-  describe '#weather' do
-    it 'returns a hash with attributes: current_weather, daily_weather, and hourly_weather' do
-      coords = WeatherFacade.coordinates("pueblo west,co")
-      data = WeatherFacade.weather(coords.lat, coords.lon)
-
-      expect(data).to be_a Hash
-      expect(data[:current_weather])
-    end
+  it '::weather' do
+    data = WeatherFacade.weather(39.73845, -104.98485)
+    expect(data).to be_a Hash
+    expect(data[:lat]).to eq 39.7385
+    expect(data[:lon]).to eq -104.9849
+    expect(data.keys).to eq([:lat, :lon, :timezone, :timezone_offset, :current, :hourly, :daily])
   end
 
-  
+  it '::coordinates' do
+    data = WeatherFacade.coordinates("palmer lake,co")
+    expect(data).to be_a Coords
+    expect(data.lat).to eq 39.101463
+    expect(data.lon).to eq -104.882056
+  end
+
+  it '::background' do
+    data = WeatherFacade.background("denver,co")
+    expect(data).to be_a Hash
+    expect(data.keys).to eq([:page, :per_page, :photos, :total_results, :next_page])
+  end
+end
